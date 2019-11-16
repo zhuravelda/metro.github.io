8cb1d9eea6e0e5b6a4bf93af84cb6b0db70bf293
metrokyiv <zhuravelda@gmail.com>
2019-11-17 00:48:28 +0200

    Пропрацював дынамыку роботы слайдера

--- source/js/main.js
+++ source/js/main.js
@@ -1,37 +1,38 @@
-// $('.header-slider').slick({
-//     autoplay: true,
-//     infinite: true,
-//     slidesToShow: 3,
-//     slidesToScroll: 1,
-//     centerPadding: 0,
-//     centerMode: true,
-//     pauseOnDotsHover: true,
-//     dots: true,
-//     arrows: false
-//   });
-
-  $('.header-slider').slick({
-    centerMode: true,
-    centerPadding: '260px',
+$('.header-slider').slick({
+    autoplay: true,
+    speed: 1300,
+    infinite: true,
     slidesToShow: 3,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '240px',
-          slidesToShow: 3
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '240px',
-          slidesToShow: 1
-        }
-      }
-    ]
+    slidesToScroll: 1,
+    centerMode: true,
+    centerPadding: '30px',
+    pauseOnDotsHover: true,
+    dots: true,
+    arrows: false
   });
+
+  // $('.header-slider').slick({
+  //   centerMode: true,
+  //   centerPadding: '50px',
+  //   slidesToShow: 3,
+  //   responsive: [
+  //     {
+  //       breakpoint: 768,
+  //       settings: {
+  //         arrows: false,
+  //         centerMode: true,
+  //         centerPadding: '240px',
+  //         slidesToShow: 3
+  //       }
+  //     },
+  //     {
+  //       breakpoint: 480,
+  //       settings: {
+  //         arrows: false,
+  //         centerMode: true,
+  //         centerPadding: '240px',
+  //         slidesToShow: 1
+  //       }
+  //     }
+  //   ]
+  // });
